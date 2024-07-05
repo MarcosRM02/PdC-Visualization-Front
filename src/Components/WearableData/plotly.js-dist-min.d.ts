e6bae833b1ec4f971ba6de8bd3d6b0619f447293
MarcosRM02 <Marcos.Redondo1@alu.uclm.es>
2024-07-05 11:13:29 +0200

    Deleting unnecesary file

--- src/Components/WearableData/plotly.js-dist-min.d.ts
+++
@@ -1,8 +0,0 @@
-// No se si este archivo debe ir aqui, pero lo pongo de momento, ya que es el unico sitio donde uso plotly.js
-
-// he hecho este archivo para poder usar plotly en el proyecto, ya que no se puede importar directamente en el proyecto
-
-declare module "plotly.js-dist-min" {
-  const Plotly: any; // Usa any o una definición de tipo más específica si es necesario
-  export = Plotly; // Ver como exportar solo relyout de plotly que es lo que me interesa de momento
-}
